f77c3b9f3fa46a774ebc02edcff92eab7dace7b1
Thee Ho <thee@berkeley.edu>
2024-06-22 14:26:52 -0700

    Add 3 supplements info

--- src/app.py
+++ src/app.py
@@ -1,5 +1,3 @@
-# pip install streamlit langchain lanchain-openai beautifulsoup4 python-dotenv chromadb
-
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.document_loaders import WebBaseLoader
@@ -11,20 +9,30 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
+from langchain.document_loaders import AsyncHtmlLoader
+from langchain.document_transformers import Html2TextTransformer
+
+
 
 load_dotenv()
+URLS = ["https://www.webmd.com/drugs/2/drug-172941/magnesium-l-threonate-oral/details", 
+        "https://www.webmd.com/vitamins/ai/ingredientmono-1126/berberine",
+        "https://www.mayoclinic.org/drugs-supplements-vitamin-d/art-20363792"
+       ]
 
-def get_vectorstore_from_url(url):
-    # get the text in document form
-    loader = WebBaseLoader(url)
-    document = loader.load()
-    
-    # split the document into chunks
+def get_vectorstore_from_url():
+    loader = AsyncHtmlLoader(URLS)
+    docs = loader.load()
+
+    html2text_transformer = Html2TextTransformer()
+    docs_transformed = html2text_transformer.transform_documents(docs)
     text_splitter = RecursiveCharacterTextSplitter()
-    document_chunks = text_splitter.split_documents(document)
+    document_chunks = text_splitter.split_documents(docs_transformed)
+    
     
     # create a vectorstore from the chunks
     vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+    # vector_store = Chroma(persist_directory="./chroma_db", embedding_function=embedding_function)
 
     return vector_store
 
@@ -69,40 +77,32 @@ def get_response(user_input):
     return response['answer']
 
 # app config
-st.set_page_config(page_title="Chat with websites", page_icon="🤖")
-st.title("Chat with websites")
+st.set_page_config(page_title="Supplement Bot", page_icon="🤖")
+st.title("Supplement Bot")
 
-# sidebar
-with st.sidebar:
-    st.header("Settings")
-    website_url = st.text_input("Website URL")
 
-if website_url is None or website_url == "":
-    st.info("Please enter a website URL")
+# session state
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello, I am a bot. How can I help you?"),
+    ]
+if "vector_store" not in st.session_state:
+    st.session_state.vector_store = get_vectorstore_from_url()    
 
-else:
-    # session state
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = [
-            AIMessage(content="Hello, I am a bot. How can I help you?"),
-        ]
-    if "vector_store" not in st.session_state:
-        st.session_state.vector_store = get_vectorstore_from_url(website_url)    
+# user input
+user_query = st.chat_input("Type your message here...")
+if user_query is not None and user_query != "":
+    response = get_response(user_query)
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    st.session_state.chat_history.append(AIMessage(content=response))
+    
+    
 
-    # user input
-    user_query = st.chat_input("Type your message here...")
-    if user_query is not None and user_query != "":
-        response = get_response(user_query)
-        st.session_state.chat_history.append(HumanMessage(content=user_query))
-        st.session_state.chat_history.append(AIMessage(content=response))
-        
-       
-
-    # conversation
-    for message in st.session_state.chat_history:
-        if isinstance(message, AIMessage):
-            with st.chat_message("AI"):
-                st.write(message.content)
-        elif isinstance(message, HumanMessage):
-            with st.chat_message("Human"):
-                st.write(message.content)
+# conversation
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.write(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.write(message.content)
